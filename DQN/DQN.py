8ca50dcf3e317aff57047502aa96b349e18d19dc
asong18 <asong18@jhu.edu>
2022-04-20 19:14:41 -0400

    updates

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -7,16 +7,18 @@ import numpy as np
 
 # need to discretize the action space. 
 class DeepQNetwork(nn.Module):
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims,
+                 n_actions):
         super(DeepQNetwork, self).__init__()
-        self.input.dims = input_dims
+        self.input_dims = input_dims
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
         self.n_actions = n_actions
         self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions) 
-        self.optimizer = optim.Adam(self.parameters(), lr = lr)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
@@ -27,6 +29,8 @@ class DeepQNetwork(nn.Module):
         x = F.relu(self.fc2(x))
         # we want the raw number because this could be negative or positive. 
         actions = self.fc3(x)
+
+        return actions
 
 
 # gamma = determines weighting of future rewards. 
