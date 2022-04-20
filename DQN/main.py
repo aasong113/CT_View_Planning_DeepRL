82fb7b2464c5dedbe0ac86b2b10add2ce9addb9f
asong18 <asong18@jhu.edu>
2022-04-20 19:48:26 -0400

    testing environment

--- DQN/main.py
+++ DQN/main.py
@@ -8,45 +8,46 @@ if __name__ == '__main__':
     agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01,
                   input_dims=[8], lr=0.001)
     scores, eps_history = [], []
-    n_games = 500
+    n_games = 1
     
-    for i in range(n_games):
-        score = 0
-        done = False
-        observation = env.reset()
-        while not done:
-            action = agent.choose_action(observation)
-            observation_, reward, done, info = env.step(action)
-            score += reward
-            agent.store_transition(observation, action, reward, 
-                                    observation_, done)
-            agent.learn()
-            observation = observation_
-        scores.append(score)
-        eps_history.append(agent.epsilon)
+    print(env.action_space)
+#     for i in range(n_games):
+#         score = 0
+#         done = False
+#         observation = env.reset()
+#         while not done:
+#             action = agent.choose_action(observation)
+#             observation_, reward, done, info = env.step(action)
+#             score += reward
+#             agent.store_transition(observation, action, reward, 
+#                                     observation_, done)
+#             agent.learn()
+#             observation = observation_
+#         scores.append(score)
+#         eps_history.append(agent.epsilon)
 
-        avg_score = np.mean(scores[-100:])
+#         avg_score = np.mean(scores[-100:])
 
-        print('episode ', i, 'score %.2f' % score,
-                'average score %.2f' % avg_score,
-                'epsilon %.2f' % agent.epsilon)
-    x = [i+1 for i in range(n_games)]
-    filename = 'lunar_lander.png'
-    plotLearning(x, scores, eps_history, filename)
+#         print('episode ', i, 'score %.2f' % score,
+#                 'average score %.2f' % avg_score,
+#                 'epsilon %.2f' % agent.epsilon)
+#     x = [i+1 for i in range(n_games)]
+#     filename = 'lunar_lander.png'
+#     plotLearning(x, scores, eps_history, filename)
 
 
-    # create figure and axis objects with subplots()
-fig,ax = plt.subplots()
-# make a plot
-ax.plot(x, scores, color="red", marker="o")
-# set x-axis label
-ax.set_xlabel("Training Steps",fontsize=14)
-# set y-axis label
-ax.set_ylabel("Scores",color="red",fontsize=14)
+#     # create figure and axis objects with subplots()
+# fig,ax = plt.subplots()
+# # make a plot
+# ax.plot(x, scores, color="red", marker="o")
+# # set x-axis label
+# ax.set_xlabel("Training Steps",fontsize=14)
+# # set y-axis label
+# ax.set_ylabel("Scores",color="red",fontsize=14)
 
-# twin object for two different y-axis on the sample plot
-ax2=ax.twinx()
-# make a plot with different y-axis using second axis object
-ax2.plot(x, eps_history,color="blue",marker="o")
-ax2.set_ylabel("Epsilon History",color="blue",fontsize=14)
-plt.show()
+# # twin object for two different y-axis on the sample plot
+# ax2=ax.twinx()
+# # make a plot with different y-axis using second axis object
+# ax2.plot(x, eps_history,color="blue",marker="o")
+# ax2.set_ylabel("Epsilon History",color="blue",fontsize=14)
+# plt.show()
